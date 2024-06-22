4f6d0335ac49d71fed41d6487f866b115753b763
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2024-06-22 13:15:07 +0200

    index.html
    Setup and skeleton of the page

---
+++ src/setup/setup.js
@@ -0,0 +1,56 @@
+const SNIADANIA = [
+    "Omlet gryczany",
+      "Budyń owsiany",
+      "Owsianka idealna",
+      "Jesienna owsianka",
+      "Kokosowa ryżanka",
+      "Słodki mus",
+      "Słodki mus z mango",
+      "Czekoladowe chia",
+      "Czekoladowy pudding",
+      "Najlepsza granola na świecie",
+      "Rawnola - granola na surowo",
+      "Śniadaniowe ciasto marchewkowe",
+      "Lody bananowe",
+      "Batony musli"]
+
+
+      const SLODKOSCI = [
+        "Baton pełen energii",
+        "Kulki mocy",
+        "Raffaelo",
+        "Sezamki",
+        "Śliwki w czekoladzie",
+        "Bomby czekoladowe",
+        "Krówki",
+        "Zielony sernik na zimno",
+        "Słońce na talerzu",
+        "Różany tort",
+        "Słodko—słono",
+        "Chlebek bananowy",
+        "Ciasto marchewkowe",
+        "Sernik jaglany",
+        "Szarlotka",
+        "Lody kokosowe"
+      ];
+      
+
+      const SLODKIE_DODATKI = [
+        "Słodki sos do śniadania",
+        "Sos czekoladowy",
+        "Nutella bez orzechów",
+        "Najprostsza pasta daktylowa",
+        "Daktylowe kawałki czekolady",
+        "Bekon z kokosa"
+      ];
+
+
+const SMOOTHIES = [
+    "Zielone smoothie",
+    "Pomarańczowe smoothie",
+    "Różowe smoothie",
+    "Fioletowe smoothie",
+    "Czekoladowe smoothie"
+  ];
+
+  export {SNIADANIA, SLODKOSCI, SLODKIE_DODATKI, SMOOTHIES}
