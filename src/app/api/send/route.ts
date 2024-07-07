37c7b31038828e1cb0c2777c22201c2c4d22da10
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2024-07-07 03:17:14 +0200

    fixes

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,23 +1,24 @@
-import { EmailTemplate } from '@/components/emailTemplate';
-import { Resend } from 'resend';
+import { EmailTemplate } from "@/components/emailTemplate";
+import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+export async function POST(request: Request) {
+  const { email, name } = await request.json();
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: "Acme <onboarding@resend.dev>",
+      to: [email],
+      subject: "hello world",
+      html: `${name} <strong>it works!</strong>`,
+    }),
+  });
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['rafal.ziolek@icloud.com'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
-
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
-
+  if (res.ok) {
+    const data = await res.json();
     return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
   }
 }
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -15,34 +15,40 @@ export function Accordion({
   );
 }
 
+interface AccordionItemProps
+  extends React.ComponentPropsWithoutRef<typeof AccordionRadix.Item> {
+  children: React.ReactNode;
+  value: string;
+}
+
 export function AccordionItem({
   children,
   value,
+  className,
   ...props
-}: {
-  children: React.ReactNode;
-  value: string;
-}) {
+}: AccordionItemProps) {
   return (
-    <AccordionRadix.Item value={value} {...props}>
+    <AccordionRadix.Item value={value} className={className} {...props}>
       {children}
     </AccordionRadix.Item>
   );
 }
-interface AccordionTriggerProps extends React.HTMLAttributes<HTMLDivElement> {
+
+interface AccordionTriggerProps
+  extends React.HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
 export const AccordionTrigger = React.forwardRef<
-  HTMLDivElement,
-  AccordionContentProps
+  HTMLButtonElement,
+  AccordionTriggerProps
 >(({ children, className, ...props }, forwardedRef) => (
   <AccordionRadix.Trigger
     className={`${className} text-xl flex items-center justify-between w-full group`}
     {...props}
     ref={forwardedRef}
   >
-    <div className="AccordionContentText">{children}</div>{" "}
+    <div>{children}</div>
     <CaretDownIcon className="transition-transform duration-300 ease-in-out group-data-[state=open]:rotate-180" />
   </AccordionRadix.Trigger>
 ));
