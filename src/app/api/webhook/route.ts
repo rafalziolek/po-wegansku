421d65b1f7852318388bc6bb9d2a8edd971d5470
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2024-07-12 18:45:54 +0200

    Update route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import { sendEmail } from "../../../utils/sendEmail"; // Adjust the import path as needed
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2024-04-10" as any,
   typescript: true,
 });
 
