c1da580cb44f1081cad953664eeeef5171fa6569
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2024-07-14 15:49:58 +0200

    Update sendEmail.ts

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail(email: string, name: string) {
   try {
     const data = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
+      from: "Nikola <nikola@chmielvegan.com>",
       to: [email],
       subject: "Thank you for your purchase!",
       html: `<p>Hello ${name},</p><p>Thank you for purchasing our ebook! Here is your download link: <a href="https://j8gqkv04whnigint.public.blob.vercel-storage.com/powegansku-naslodko-nikolachmiel-LAcTGscBCfMuFL10EWy9CROc6cB3Zt.pdf?download=1" download>Download Ebook</a></p>`,
